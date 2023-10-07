700fce535364fb24650491309b4a5f10b839758d
Sayapulu <lulamalik9@gmail.com>
2023-10-06 23:48:59 -0700

    Praktikum Perulangan

---
+++ 02 pemasukan dan keluaran/04 perulangan/loop4.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+     char ulangi = 'y';
+     int counter = 0;
+
+     // perulangan while
+     while (ulangi == 'y')
+     {
+        cout << "Apakah kamu sayang aku?" << endl;
+        cout << "jawab (y/t): ";
+        cin >> ulangi;
+
+        // increment counter
+        counter++;
+     }
+
+     cout << "\n\n----------\n";
+     cout << "Anda kurang beruntung!" <<endl;
+     cout << "Kamu berusaha sebanyak " << counter << " Kali." <<endl;
+     ;
+
+     return 0;
+}
